1f6499d4665f07ecc85fd012142c7c838be98bc7
SK8x666 <1433023326@qq.com>
2025-03-29 03:11:19 +0800

    1

---
+++ Src/Client/Assets/Levels/login.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class login : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		Network.NetClient.Instance.Init("127.0.0.1", 8000);
+        Network.NetClient.Instance.Connect();
+
+		SkillBridge.Message .NetMessage msg = new SkillBridge.Message .NetMessage ();	
+		msg.Request=new SkillBridge.Message .NetMessageRequest ();	
+		SkillBridge.Message.FirstTestRequest firstTestRequest =new SkillBridge.Message.FirstTestRequest();
+		firstTestRequest.Helloword = "Hello,word";
+
+		msg.Request.firstRequest=firstTestRequest;	
+
+		Network.NetClient.Instance.SendMessage(msg);
+    }
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
